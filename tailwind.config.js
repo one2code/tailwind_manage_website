8c8ea609db22ff9389421a9a284aac635734e315
Travis Bennett <t_bennett_a@protonmail.com>
2022-04-27 12:20:06 -0400

    Changed all instances of text-white to a custom variable and extended it in the tailwind.config file for maintainability

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,7 @@ module.exports = {
         veryDarkBlue: 'hsl(233, 12%, 13%)',
         veryPaleRed: 'hsl(13, 100%, 96%)',
         veryLightGray: 'hsl(0, 0%, 98%)',
+        textPrimary:'#fff',
       },
     },
   },
